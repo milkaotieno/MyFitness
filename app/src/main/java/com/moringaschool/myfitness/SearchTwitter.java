79e58480cc51d07c4f1b93605d8586d485f73317
milkaotieno <milka.otieno@student.moringaschool.com>
2022-04-08 20:00:53 +0300

    adding twitter lifecycle

---
+++ app/src/main/java/com/moringaschool/myfitness/SearchTwitter.java
@@ -0,0 +1,39 @@
+package com.moringaschool.myfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchTwitter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_twitter);
+    }
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+    @Override
+    protected  void onPause () {
+
+        super.onPause();
+    }
+    @Override
+    protected  void onStop() {
+
+        super.onStop();
+    }
+    @Override
+    protected  void onRestart() {
+
+        super.onRestart();
+    }
+    @Override
+    protected  void onDestroy() {
+
+        super.onDestroy();
+    }
+}
